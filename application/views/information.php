68823724f4e136838cd8d298b8f559795fddbd7f
DevenCC <deven.collette@gmail.com>
2014-11-13 18:13:12 -0500

    fixed typos
    - playoff / playoffs
    - leave / leave
    - /n / </br>

--- application/views/information.php
+++ application/views/information.php
@@ -1,13 +1,14 @@
 <div class="container">
 	<h2>More about the league</h2>
 		<p>
-			The Chambly Ball Hockey League started in Chambly, QC in 1980 as weekly friendly pick-up games. Today the league consists of 5 teams, where 4 teams, on rotation, play each other (2 games) every Saturday morning at 10:00 with the 5th team's players acting as referees. Seasons start in October and end in May. Playoff, then follow a week after the last regular season.\n
+			The Chambly Ball Hockey League started in Chambly, QC in 1980 as weekly friendly pick-up games. Today the league consists of 5 teams, where 4 teams, on rotation, play each other (2 games) every Saturday morning at 10:00 with the 5th team's players acting as referees. Seasons start in October and end in May. Playoffs then follow a week after the last regular season.
+		</br>
 			The game is played 4 on 4 outside on an asphalt surface. Rain or shine ...or snowstorm, matches are held regardless of weather conditions.
 		</p>
 		</br>
 	<h2>Want to join</h2>
 		<p>
-			Leave in the Montreal/ South Shore area? Interested in joining the league? Don't hesitate to ask. Before the first game of the year, a rookie game is held open for anyone interested to play. The team captains are there to scout for the season's draft usually held right after. For dates and anymore information contact us by email at info@chamblyballhockey.com .
+			Live in the Montreal/ South Shore area? Interested in joining the league? Don't hesitate to ask. Before the first game of the year, a rookie game is held open for anyone interested to play. The team captains are there to scout for the season's draft usually held right after. For dates and anymore information contact us by email at info@chamblyballhockey.com .
 		</p>
 		</br>
 	<h2>Information about the site</h2>
